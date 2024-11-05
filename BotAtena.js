1f8f5ac489da424ee1d69a1661cb136ff8500028
inaciopvhro <inaciopvhro@hotmail.com>
2024-11-05 19:51:45 -0400

    gravar usuarios

--- BotAtena.js
+++ BotAtena.js
@@ -28,7 +28,7 @@ function delay(t, v) {
 const createConnection = async () => {
 	return await mysql.createConnection({
 		host: '147.79.86.208',
-		user: 'root',
+		user: 'inaciolocal',
 		password: 'Inacio@2628',
 		database: 'BancoBot'
 	});
@@ -47,9 +47,9 @@ const getUser = async (msgfom) => {
 	return false;
 };
 
-const setUser = async (msgfom, nome) => {
+const setUser = async (msgfom, nome, gruponome) => {
 	const connection = await createConnection();
-	const [rows] = await connection.execute('INSERT INTO `contatos` (`id`, `contato`, `nome`) VALUES (NULL, ?, ?)', [msgfom, nome]);
+	const [rows] = await connection.execute('INSERT INTO `contatos` (`id`, `contato`, `nome`, `grupo`) VALUES (NULL, ?, ?, ?)', [msgfom, nome, gruponome]);
   delay(1000).then(async function() {
 		await connection.end();
 		delay(500).then(async function() {
@@ -524,11 +524,12 @@ client.on('group_join', async (notification) => {
   try{
     const contact = await client.getContactById(notification.id.participant)
     const nomeContato = (contact.pushname === undefined) ? contact.verifiedName : contact.pushname;
+    const grupoid = await client.getChatById(notification.id.remote);
     const user = notification.id.participant.replace(/\D/g, '');
     const getUserFrom = await getUser(user);
-
+    
     if (getUserFrom === false) {
-      await setUser(user, nomeContato);
+      await setUser(user, nomeContato, grupoid.name);
       console.log('Usuário armazenado: ' + user + ' - ' + nomeContato)
     }
 
